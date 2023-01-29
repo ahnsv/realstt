de702a600b2d2e05f449f88b85696b2a545f7161
ahnsv <ahnsv@bc.edu>
2023-01-29 16:41:35 +0900

    refactor(realstt): enhance logging

--- realstt/service.py
+++ realstt/service.py
@@ -9,7 +9,7 @@ from bentoml.io import File, JSON
 
 from realstt.runnables.audio_transcribe import AudioTranscriber
 
-model = whisper.load_model("base")
+model = whisper.load_model("base") # TODO: replace this with custom trained model from MLflow
 runner = bentoml.Runner(AudioTranscriber, name="whisper",
                         runnable_init_params={"model": model})
 
@@ -19,21 +19,22 @@ logger = logging.getLogger(f"bentoml")
 
 
 @contextlib.contextmanager
-def log_execution_time():
+def log_execution_time(namespace: str):
     start_time = time.time()
     yield
     end_time = time.time()
     execution_time = end_time - start_time
-    logger.info(f"Executed in {execution_time:.2f} seconds.")
+    logger.info(f"Executed {namespace=} in {execution_time:.2f} seconds.")
 
 
 @svc.api(input=File(), output=JSON())
 async def predict(audio: io.BytesIO):
-    path = f"/tmp/audio.mp4"
-    with open(path, "wb") as f:
-        f.write(audio.read())
+    with log_execution_time("writing temp file"):
+        path = f"/tmp/audio.mp4"
+        with open(path, "wb") as f:
+            f.write(audio.read())
 
-    with log_execution_time():
+    with log_execution_time("inference"):
         transcript = await runner.transcribe_audio.async_run(path)
     transcript_text = transcript["text"]
     return transcript_text
